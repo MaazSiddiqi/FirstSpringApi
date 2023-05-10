99b72aa1fd2e8835638319354e1a6046a55ae72a
Maaz Ali <maazali22@gmail.com>
2023-05-10 17:44:50 -0400

    bootstrapped spring file structure and made animal model

---
+++ src/main/java/com/example/firstjavaspringapi/FirstJavaSpringApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstjavaspringapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstJavaSpringApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstJavaSpringApiApplication.class, args);
+    }
+
+}
