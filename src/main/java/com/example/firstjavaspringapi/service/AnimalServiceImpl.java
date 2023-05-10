e27acab386f6aa24df2d21f2a7c9bc5dc4496ca6
Maaz Ali <maazali22@gmail.com>
2023-05-10 18:48:30 -0400

    created implementations of new intergace

--- src/main/java/com/example/firstjavaspringapi/service/AnimalServiceImpl.java
+++ src/main/java/com/example/firstjavaspringapi/service/AnimalServiceImpl.java
@@ -1,15 +1,35 @@
 package com.example.firstjavaspringapi.service;
 
-import com.example.firstjavaspringapi.controller.AnimalController;
 import com.example.firstjavaspringapi.model.Animal;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 @Service
-public class AnimalService implements IAnimalService {
+public class AnimalServiceImpl implements AnimalService {
 
+    ArrayList<Animal> animals = new ArrayList<>(Arrays.asList(new Animal(1, "Dog", 3), new Animal(2, "Cat", 2)));
 
+    @Override
     public Animal getAnimalbyId(long id) {
-        return new Animal(id, "Dog", 3);
+        for (Animal animal : animals) {
+            if (animal.getId() == id) {
+                return animal;
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public ArrayList<Animal> getAllAnimals() {
+        return animals;
+    }
+
+    @Override
+    public Animal addAnimal(Animal animal) {
+        animals.add(animal);
+        return animal;
     }
 }
